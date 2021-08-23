443dda3c2551452862045ff4ac818e7ebf924229
Arron Moss <arron.moss@zero1.co.uk>
2021-08-23 22:39:04 +0100

    Update migrate.sh

--- mdoq/migrate.sh
+++ mdoq/migrate.sh
@@ -110,24 +110,19 @@ bin/magento config:set smtp/module/name 'Julia Prestia'
 #bin/magento config:set smtp/module/subscribe 1
 bin/magento config:set smtp/general/enabled 1
 
-# /* Set new homepage */
+# Set new homepage
 bin/magento config:set web/default/cms_home_page home_m2
-
 
 bin/magento config:set twofactorauth/general/enable 0
 
 bin/magento config:set cataloginventory/item_options/min_sale_qty 1
 bin/magento config:set cataloginventory/item_options/min_qty 1
 
-# /* Content Changes */
+# Content Changes 
 bin/magento config:set web/default/cms_home_page home_m2
-echo "USE ${m2db_database}; update cms_page set identifier = 'customer-service-old' where identifier = 'customer-service'; update cms_page set identifier = 'customer-service' where identifier = 'customer-service_m2'; | mysql -h ${m2db_host} -u ${m2db_user} -p${m2db_password} ${m2db_database};"
-echo "USE ${m2db_database}; update cms_page set identifier = 'location-directions-old' where identifier = 'location-directions'; update cms_page set identifier = 'location-directions' where identifier = 'location-directions_m2'; | mysql -h ${m2db_host} -u ${m2db_user} -p${m2db_password} ${m2db_database};"
-echo "USE ${m2db_database}; update cms_block set identifier = 'footer_links_custom_old' where identifier = 'footer_links_custom'; update cms_page set identifier = 'footer_links_custom' where identifier = 'footer_links_custom_m2'; | mysql -h ${m2db_host} -u ${m2db_user} -p${m2db_password} ${m2db_database};"
-
-# Change title of click and collect
-bin/magento config:set carriers/instore/name 'Pick Up Locations'
-bin/magento config:set carriers/instore/title 'Pick Up My Order'
+echo "USE ${m2db_database}; update cms_page set identifier = 'customer-service-old' where identifier = 'customer-service'; update cms_page set identifier = 'customer-service' where identifier = 'customer-service_m2';" | mysql -h ${m2db_host} -u ${m2db_user} -p${m2db_password} ${m2db_database};
+echo "USE ${m2db_database}; update cms_page set identifier = 'location-directions-old' where identifier = 'location-directions'; update cms_page set identifier = 'location-directions' where identifier = 'location-directions_m2';" | mysql -h ${m2db_host} -u ${m2db_user} -p${m2db_password} ${m2db_database};
+echo "USE ${m2db_database}; update cms_block set identifier = 'footer_links_custom_old' where identifier = 'footer_links_custom'; update cms_page set identifier = 'footer_links_custom' where identifier = 'footer_links_custom_m2';" | mysql -h ${m2db_host} -u ${m2db_user} -p${m2db_password} ${m2db_database};
 
 # https://zero1.teamwork.com/#/tasks/24020245 Google Analytics
 # Not required, pulling value from M1
@@ -140,9 +135,6 @@ bin/magento config:set -- catalog/frontend/flat_catalog_product 0
 
 # https://zero1.teamwork.com/#/tasks/24635208
 bin/magento config:set sales/totals_sort/loworderfee 35
-
-# Instore Collect
-#bin/magento config:set carriers/instore/active 1
 
 # https://zero1.teamwork.com/#/tasks/24020248  - THROWS EXCEPTION 
 # Unable to serialize value. Error: Malformed UTF-8 characters, possibly incorrectly encoded
@@ -173,3 +165,8 @@ bin/magento maintenance:disable
 curl -X POST --data-urlencode "payload={\"channel\": \"#barrdisplay\", \"username\": \"webhookbot\", \"text\": \"Prod Instance Migration Completed\", \"icon_emoji\": \":partying_face:\"}" https://hooks.slack.com/services/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
 echo "migration complete" > mdoq/.migrate
 
+# Change title of click and collect
+# These seem to fail all the time for some reason, moving to the end.
+bin/magento config:set -- carriers/instore/active 1
+bin/magento config:set -- carriers/instore/name 'Pick Up Locations'
+bin/magento config:set -- carriers/instore/title 'Pick Up My Order'
